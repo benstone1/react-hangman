8937c1dd7d6bd75a2981ec0e100d4b7b1907e5d9
Ben Stone <benjamin.h.stone@gmail.com>
2019-11-13 21:45:24 -0500

    initial structure

---
+++ src/Board.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Board extends React.Component {
+  render() {
+    return (
+      <div>
+        <p> This is the Hangman Board </p>
+      </div>
+    )
+  }
+}
+
+export default Board;
