96379562e3d9e00d8fdd96afc69b9d129960f2b4
Ben Stone <benjamin.h.stone@gmail.com>
2020-03-13 21:05:20 -0400

    create board scaffold

--- src/Board.js
+++ src/Board.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+const initialState = {
+  displayWord: ""
+}
+
 class Board extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = initialState
+  }
   render() {
     return (
       <div>
         <p> This is the Hangman Board </p>
+        <p> _ _ _ _</p>
       </div>
     )
   }
